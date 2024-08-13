e432f997a3c44325460e7a9800ae4470c46e30cd
Seven <ahmadaliev.sd@gmail.com>
2024-08-13 21:19:13 +0900

    users model error fixed

--- apps/api/src/models/users/graphql/entity/user.entity.ts
+++ apps/api/src/models/users/graphql/entity/user.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType } from '@nestjs/graphql'
+import { Field,ObjectType } from '@nestjs/graphql'
 import { User as UserType } from '@prisma/client'
 import { RestrictProperties } from 'src/common/dtos/common.input'
 
@@ -7,6 +7,7 @@ export class User implements RestrictProperties<User, UserType> {
   uid: string
   createdAt: Date
   updatedAt: Date
+  @Field({ nullable: true })
   name: string
   // Todo Add below to make optional fields optional.
   // @Field({ nullable: true })
